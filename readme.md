c73baf87009d09a2dd3812d6f32d9c5781f50aa9
Pepijn Bruinaars <pepijnbruinaars@xs4all.nl>
2025-06-23 11:46:21 +0200

    multi-head attention

--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
 # Transformers from scratch
 
-Wanted to do something on my Friday night, so I decided to turn off code suggestions and started working on this from scratch in PyTorch, using the original Attention Is All You Need paper as reference.
+Wanted to do something on my Friday night, so I decided to turn off code suggestions and started working on this from scratch in PyTorch, using the original Attention Is All You Need paper as reference. Also added a lot of type hints to make it easier to read. 3B1B's videos on transformers were also a great help in understanding the matrix multiplications.
 
 I'll look into what I can use it for later.
--- src/transformer/components/MultiHeadAttention.py
+++ src/transformer/components/MultiHeadAttention.py
@@ -1,3 +1,4 @@
+from typing import Optional, Tuple
 import torch
 import torch.nn as nn
 
@@ -5,24 +6,33 @@ import math
 
 
 def scaled_dot_product_attention(
-    Q: torch.FloatTensor, K: torch.FloatTensor, V: torch.FloatTensor
-):
+    Q: torch.FloatTensor,
+    K: torch.FloatTensor,
+    V: torch.FloatTensor,
+    mask: Optional[torch.Tensor] = None,
+    dropout: Optional[nn.Dropout] = None,
+) -> Tuple[torch.Tensor, torch.Tensor]:
     """Function that calculates the scaled dot product attention for a set of
-
+    Attention(Q,K,V) = softmax((Q @ K^T) / sqrt(d_k)) @ V
     Args:
         Q (FloatTensor): The query vector
         K (FloatTensor): The keys vector
         V (FloatTensor): The values vector
     """
     # 1. Calculate scores and normalize
-    scores = torch.matmul(Q, K.T)
-    normalized_scores = scores / math.sqrt(K.shape[0])
+    scores = torch.matmul(Q, K.transpose(-2, -1))
+    normalized_scores = scores / math.sqrt(K.shape[-1])
+    if mask is not None:
+        normalized_scores.masked_fill_(mask == 0, -1e12)
 
     # 2. Apply softmax to get probabilities
     probabilities = torch.softmax(normalized_scores, dim=-1)
+    if dropout is not None:
+        probabilities = dropout(probabilities)
+
     output = torch.matmul(probabilities, V)
 
-    return output
+    return output, probabilities
 
 
 class MultiHeadAttention(nn.Module):
@@ -32,7 +42,7 @@ class MultiHeadAttention(nn.Module):
         nn (_type_): _description_
     """
 
-    def __init__(self, d_model: int, n_heads: int) -> None:
+    def __init__(self, d_model: int, n_heads: int, dropout: float) -> None:
         super(MultiHeadAttention, self).__init__()
 
         # Assert dimensions
@@ -41,9 +51,49 @@ class MultiHeadAttention(nn.Module):
 
         self.d_model = d_model
         self.n_heads = n_heads
-        self.d_k = d_model / n_heads
+        self.d_k = d_model // n_heads
 
         # Query, key and value weights
-        self.Q = nn.Linear(d_model, d_model)
-        self.K = nn.Linear(d_model, d_model)
-        self.V = nn.Linear(d_model, d_model)
+        self.W_Q = nn.Linear(d_model, d_model)
+        self.W_K = nn.Linear(d_model, d_model)
+        self.W_V = nn.Linear(d_model, d_model)
+
+        # Output weights and dropout
+        self.W_O = nn.Linear(d_model, d_model)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(
+        self,
+        Q: torch.FloatTensor,
+        K: torch.FloatTensor,
+        V: torch.FloatTensor,
+        mask: torch.Tensor,
+    ):
+        # Get Q', K', and V'
+        query: torch.Tensor = self.W_Q(Q)
+        key: torch.Tensor = self.W_K(K)
+        value: torch.Tensor = self.W_V(V)
+
+        # Split into 'pre-heads'
+        query = query.view(
+            query.shape[0], query.shape[1], self.n_heads, self.d_k
+        ).transpose(1, 2)
+        key = key.view(key.shape[0], key.shape[1], self.n_heads, self.d_k).transpose(
+            1, 2
+        )
+        value = value.view(
+            value.shape[0], value.shape[1], self.n_heads, self.d_k
+        ).transpose(1, 2)
+
+        x, attention_scores = scaled_dot_product_attention(
+            Q,
+            K,
+            V,
+            mask,
+            self.dropout,
+        )
+
+        # (Batch, len, n_heads, d_k) -> (Batch, len, d_model)
+        x = x.transpose(1, 2).contiguous().view(x.shape[0], -1, self.n_heads * self.d_k)
+
+        return self.W_O(x)
