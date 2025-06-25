0de84320518397c97b95a63b92ffb7d8d567d3f2
Pepijn Bruinaars <pepijnbruinaars@xs4all.nl>
2025-06-25 16:21:47 +0200

    full transformer with weight initialization, more clear parameter names and init documentation

--- src/main.py
+++ src/main.py
@@ -2,19 +2,29 @@ from transformer import Transformer
 
 
 def main() -> None:
-    # From the paper
-    encoder_blocks = 6
+    # Model parameters from the paper
+    n_blocks = 6
     d_model = 512
     d_ff = 2048
     n_heads = 8
     dropout = 0.1
 
+    # Dataset dependent parameters
+    source_length = 100
+    target_length = 100
+    source_vocabulary_size = 10000
+    target_vocabulary_size = 10000
+
     transformer = Transformer(
-        encoder_blocks=encoder_blocks,
+        n_blocks=n_blocks,
         d_model=d_model,
         d_ff=d_ff,
         n_heads=n_heads,
         dropout=dropout,
+        source_length=source_length,
+        target_length=target_length,
+        source_vocabulary_size=source_vocabulary_size,
+        target_vocabulary_size=target_vocabulary_size,
     )
     print(transformer)
 
--- src/transformer/transformer.py
+++ src/transformer/transformer.py
@@ -1,23 +1,106 @@
+import torch
 import torch.nn as nn
 
-from transformer.components import Encoder, InputEmbedding, PositionalEncoding
+from transformer.components import (
+    Encoder,
+    Decoder,
+    InputEmbedding,
+    PositionalEncoding,
+    ProjectionLayer,
+)
+
+
+def _initialize_weights(module: nn.Module) -> None:
+    """Private function to initialize the weights of the transformer model using Xavier initialization.
+
+    Args:
+        module (nn.Module): The module to initialize.
+    """
+    for p in module.parameters():
+        # We only want to initialize weights
+        if p.dim() > 1:
+            nn.init.xavier_uniform_(p)
 
 
 class Transformer(nn.Module):
     """Transformer network architecture."""
 
     def __init__(
-        self, encoder_blocks: int, d_model: int, d_ff: int, n_heads: int, dropout: float
+        self,
+        n_blocks: int,
+        d_model: int,
+        d_ff: int,
+        n_heads: int,
+        dropout: float,
+        source_length: int,
+        target_length: int,
+        source_vocabulary_size: int,
+        target_vocabulary_size: int,
     ) -> None:
-        """Initialize the transformer architecture
+        """Create a new Transformer model.
+
+        This model consists of an encoder and a decoder, each consisting of `n_blocks` blocks. The encoder processes the input sequence, while the decoder generates the output sequence based on the encoded input.
 
         Args:
-            d_model (int): The embedding dimension
+            n_blocks (int): The number of encoder/decoder blocks in the encoder/decoder
+            d_model (int): The dimensionality of the embedding space in the model. Can be thought of as the "internal resolution" of the model.
+            d_ff (int): The dimensionality of the feed-forward network in the model. Typically, `d_ff` > `d_model` to allow for complex transformations.
+            n_heads (int): The number of attention heads in the multi-head attention mechanism. This allows the model to focus on different parts of the input sequence simultaneously. Must be a divisor of `d_model`.
+            dropout (float): Probability of dropout to apply to the model. Is used throughout the model to prevent overfitting.
+            source_length (int): The maximum length of the input sequence that the model can handle. This is used to create positional encodings and input embeddings.
+            target_length (int): The maximum length of the output sequence that the model can handle. This is used to create positional encodings and input embeddings.
+            source_vocabulary_size (int): The size of the vocabulary for the input sequence. This is used to create the input embedding layer.
+            target_vocabulary_size (int): The size of the vocabulary for the output sequence. This is used to create the output embedding layer and the projection layer.
         """
         super(Transformer, self).__init__()
-        self.enc_embedding = InputEmbedding(512, 5_000)
-        self.positional_encoding = PositionalEncoding(d_model, 10, 0.0)
-        self.encoder = Encoder(encoder_blocks, d_model, d_ff, n_heads, dropout)
+        self.encoder_embedding = InputEmbedding(d_model, source_vocabulary_size)
+        self.decoder_embedding = InputEmbedding(d_model, target_vocabulary_size)
+        self.encoder_positional_encoding = PositionalEncoding(
+            d_model, source_length, dropout
+        )
+        self.decoder_positional_encoding = PositionalEncoding(
+            d_model, target_length, dropout
+        )
+        self.encoder = Encoder(
+            n_blocks=n_blocks,
+            d_model=d_model,
+            d_ff=d_ff,
+            n_heads=n_heads,
+            dropout=dropout,
+        )
+        self.decoder = Decoder(
+            n_blocks=n_blocks,
+            d_model=d_model,
+            d_ff=d_ff,
+            n_heads=n_heads,
+            dropout=dropout,
+        )
+        self.projection_layer = ProjectionLayer(d_model, target_vocabulary_size)
+
+        # Initialize parameters using He initialization
+        _initialize_weights(self)
+
+    def encode(self, x: torch.Tensor, mask: torch.Tensor) -> torch.Tensor:
+        """Encode the input sequence."""
+        x = self.encoder_embedding(x)
+        x = self.encoder_positional_encoding(x)
+        return self.encoder(x, mask)
+
+    def decode(
+        self,
+        x: torch.Tensor,
+        encoder_output: torch.Tensor,
+        encoder_mask: torch.Tensor,
+        decoder_mask: torch.Tensor,
+    ) -> torch.Tensor:
+        """Decode the input sequence."""
+        x = self.decoder_embedding(x)
+        x = self.decoder_positional_encoding(x)
+        return self.decoder(x, encoder_output, encoder_mask, decoder_mask)
+
+    def project(self, x: torch.Tensor) -> torch.Tensor:
+        """Project the output to the vocabulary size."""
+        return self.projection_layer(x)
 
     def forward(self):
         raise NotImplementedError()
